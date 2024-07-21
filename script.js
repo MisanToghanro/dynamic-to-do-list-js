9b7114c40c30aad3804a9a57c41994ac23543293
MisanToghanro <tmisan657@gmail.com>
2024-07-21 20:50:02 +0100

    add script.js

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ const addButton = document.getElementById("add-task-btn");
 const taskInput = document.getElementById("task-input");
 const taskList = document.getElementById("task-list");
 
+
+function loadTasks(){
+    const storedTasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+    storedTasks.forEach(taskText => {
+        addTask(taskText , false)
+    });
+}
+
+
 //create a function for the add task button
 function addTask(){
 
@@ -26,22 +35,42 @@ listItems.textContent = taskText;
 //create button for removing tasks
 const removeTaskButton = document.createElement("button");
 removeTaskButton.textContent = "Remove";
-removeTaskButton.className = "remove-btn";
+removeTaskButton.classList.add = "remove-btn";
 
 //add event listner to removeTaskButton to remove list items from the task list when triggered
 removeTaskButton.addEventListener("click" , function(){
     taskList.removeChild(listItems);
+    removeTaskButtonFromLocalStorage(taskText);
 });
 
 //append the removTaskButton to ListItems , then append ListItems to taskList
 listItems.appendChild(removeTaskButton);
 taskList.appendChild(listItems);
 
+if(save) {
+    saveTaskToLocalStorage(taskText);
+}
+
 //clear input field
 taskInput.value = "";
 };
-
 }
+
+//save a task to localstorage
+function saveTaskToLocalStorage(taskText) {
+    const storedTasks = JSON.parse( localStorage.getItem("tasks") || "[]");
+    storedTasks.push(taskText);
+    localStorage.setItem("tasks" , JSON.stringify(storedTasks));
+}
+
+//remove a task from local storage 
+function removeTaskButtonFromLocalStorage(taskText){
+    let storedTasks = JSON.parse(localStorage.getItem ("tasks") || "[]");
+    storedTasks = storedTasks.filter(task => task !== taskText);
+    localStorage.setItem("tasks" , JSON.stringify(storedTasks));
+}
+
+
 //add event listners
 addButton.addEventListener( "click" , addTask);
 
@@ -51,6 +80,6 @@ taskInput.addEventListener("keypress" , function(event){
     }
 });
 
-
+loadTasks();
 });
 
